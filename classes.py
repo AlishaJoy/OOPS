9c2ecb8ceb8e7fc4062b911c96993880325d8653
Alishajoy <alishajoy.2002@gmail.com>
2023-05-28 12:18:48 +0530

    done

--- classes.py
+++ classes.py
@@ -14,8 +14,27 @@ class employee:
         self.fn = first
         self.ln = last
         self.s = salary
-        
+    def fullname(self):
+        return '{} {} '.format(self.fn,self.ln)
+    
+    
 emp1 = employee('freddy','johnson',69900)  
 emp2 = employee('greddy','markston',99000)      
 print(emp1.fn)
 print(emp2.s)
+
+# methods
+#to print full name manually
+
+print('{} {}'.format(emp1.fn,emp2.ln))
+
+
+
+# create a method to print full name
+#class employee:
+ #   def fullname(self):
+  #      return '{} {} '.format(self.fn,self.ln)
+    
+print(emp2.fullname())
+# or we can print as 
+print(employee.fullname(emp1))
